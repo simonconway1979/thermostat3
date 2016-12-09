c75ad386d49fea5a4d8ce0459eb196e27e95a631
Simon <simon.conway@condaal.com>
2016-12-09 09:58:26 +0000

    Added some refactoring

--- interface.js
+++ interface.js
@@ -1,38 +1,47 @@
+var increaseTemp = $('#increaseTemp')
+var decreaseTemp = $('#decreaseTemp')
+var reset = $('#reset')
+var powerSavingOn = $('#powerSavingOn')
+var powerSavingOff = $('#powerSavingOff')
+var powerSavingStatus = $('#powerSavingStatus')
+var temperature = $('#temperature')
+
+
 $(document).ready(function() {
   var thermostat = new Thermostat();
   $('#temperature').text(thermostat.temp);
   updateTemperature();
 
-  $('#increaseTemp').click(function(){
+  increaseTemp.click(function(){
     thermostat.increaseTemp();
     updateTemperature();
   });
 
-  $('#decreaseTemp').click(function() {
+  decreaseTemp.click(function() {
     thermostat.decreaseTemp();
     updateTemperature();
   });
 
-  $('#reset').click(function(){
+  reset.click(function(){
     thermostat.resetTemp();
     updateTemperature();
   });
 
-  $('#powerSavingOn').click(function() {
+  powerSavingOn.click(function() {
    thermostat.powerSavingOn();
-   $('#power-saving-status').text('on')
+   power-saving-status.text('on')
    updateTemperature();
  })
 
- $('#powerSavingOff').click(function() {
+ powerSavingOff.click(function() {
    thermostat.powerSavingOff();
-   $('#power-saving-status').text('off')
+   power-saving-status.text('off')
    updateTemperature();
  })
 
   function updateTemperature() {
-    $('#temperature').text(thermostat.temp);
-    $('#temperature').attr('class', thermostat.energyUsage());
+    temperature.text(thermostat.temp);
+    temperature.attr('class', thermostat.energyUsage());
   };
 
 });
